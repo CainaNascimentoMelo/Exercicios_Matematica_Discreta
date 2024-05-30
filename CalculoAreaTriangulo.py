762a1e60c7c10e69e266861b17df585c65723342
CainaNascimentoMelo <caina.melo@fatec.sp.gov.br>
2024-05-29 23:19:15 -0300

    Adiciona cálculo da Área do Triângulo

---
+++ CalculoAreaTriangulo.py
@@ -0,0 +1,6 @@
+def area_triangulo(base, altura):
+    return base * altura / 2
+
+base = float(input("Digite a base do triângulo: "))
+altura = float(input("Digite a altura do triângulo: "))
+print(f"A área do triângulo é {area_triangulo(base, altura)}")
